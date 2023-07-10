84714866de03a13bbb96601de07ece4a0aa9f38c
JonNir <jonathan.nir@mail.huji.ac.il>
2023-07-10 13:42:00 +0300

    implement basic Tobii data parsers

---
+++ DataParsers/TobiiEyeTrackerHDF5Parser.py
@@ -0,0 +1,10 @@
+from DataParsers.BaseEyeTrackerParser import BaseEyeTrackerParser
+
+
+class TobiiEyeTrackerHDF5Parser(BaseEyeTrackerParser):
+    """
+    Parses eye-tracking data based on the HDF5 format exported by Tobii eye-tracker and PsychoPy.
+    """
+
+    raise NotImplementedError("This parser is not implemented yet.")
+
