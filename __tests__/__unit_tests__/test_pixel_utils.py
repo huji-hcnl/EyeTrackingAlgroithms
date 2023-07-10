3c73ce340d49a0dfcc4acbb7c36d7cd1f7a382d0
Jon Nir <51592321+JonNir1@users.noreply.github.com>
2023-07-10 16:11:50 +0300

    Utils branch (#6)
    * Update README.md
    
    * create constants.py
    
    * create ScreenMonitor.py
    
    * create experiment_config.py
    
    * implement basic Tobii data parsers
    
    * add todo
    
    * add unit tests folder
    
    * add util functions for pixels
    
    * calculate azimuth between 2 pixels

---
+++ __tests__/__unit_tests__/test_pixel_utils.py
@@ -0,0 +1,41 @@
+import unittest
+import numpy as np
+
+from Utils import pixel_utils as pixel_utils
+
+
+class TestUtils(unittest.TestCase):
+
+    def test_calculate_euclidean_distances(self):
+        sqrt2 = np.sqrt(2)
+        x_coords = np.arange(0, 5)
+        y_coords1 = np.arange(0, 5)
+        self.assertTrue(np.array_equal(np.array([sqrt2, sqrt2, sqrt2, sqrt2]),
+                                       pixel_utils.calculate_euclidean_distances(x_coords, y_coords1),
+                                       equal_nan=True))
+        y_coords2 = np.zeros_like(x_coords)
+        self.assertTrue(np.array_equal(np.array([1, 1, 1, 1]),
+                                       pixel_utils.calculate_euclidean_distances(x_coords, y_coords2),
+                                       equal_nan=True))
+        y_coords3 = y_coords1.copy().astype(float)
+        y_coords3[2] = np.nan
+        self.assertTrue(np.array_equal(np.array([sqrt2, np.nan, np.nan, sqrt2]),
+                                       pixel_utils.calculate_euclidean_distances(x_coords, y_coords3),
+                                       equal_nan=True))
+        y_coords4 = y_coords1[:-1].copy()
+        self.assertRaises(AssertionError, pixel_utils.calculate_euclidean_distances, x_coords, y_coords4)
+
+    def test_calculate_azimuth(self):
+        # angles are counter-clockwise from the positive x-axis, with y-axis pointing down
+        self.assertEqual(0, pixel_utils.calculate_azimuth(p1=(0, 0), p2=(0, 0), use_radians=False))
+        self.assertEqual(45, pixel_utils.calculate_azimuth(p1=(0, 0), p2=(1, -1), use_radians=False))
+        self.assertEqual(315, pixel_utils.calculate_azimuth(p1=(0, 0), p2=(1, 1), use_radians=False))
+        self.assertEqual(90, pixel_utils.calculate_azimuth(p1=(1, 1), p2=(1, -1), use_radians=False))
+        self.assertEqual(180, pixel_utils.calculate_azimuth(p1=(1, 1), p2=(1, -1),
+                                                            use_radians=False, zero_direction='S'))
+
+        self.assertEqual(np.pi * 3 / 4, pixel_utils.calculate_azimuth(p1=(1, 1), p2=(-2, -2), use_radians=True))
+        self.assertEqual(np.pi, pixel_utils.calculate_azimuth(p1=(1, 0), p2=(-1, 0), use_radians=True))
+        self.assertEqual(np.pi * 5 / 4, pixel_utils.calculate_azimuth(p1=(1, 0), p2=(0, 1), use_radians=True))
+        self.assertEqual(np.pi * 3 / 4, pixel_utils.calculate_azimuth(p1=(1, 0), p2=(0, 1),
+                                                                      use_radians=True, zero_direction='N'))
