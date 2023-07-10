84342933921da24b99d5f16ee9c8302f77ba9b6a
JonNir <jonathan.nir@mail.huji.ac.il>
2023-07-10 13:22:25 +0300

    create constants.py

---
+++ constants.py
@@ -0,0 +1,31 @@
+"""
+List of all the constants used as field names, column names, etc.
+"""
+
+MILLISECONDS_PER_SECOND = 1000
+MICROSECONDS_PER_SECOND = 1000000
+MICROSECONDS_PER_MILLISECOND = 1000
+
+TRIAL = "trial"
+TIME = "time"
+TRIGGER = "trigger"
+TARGET = "target"
+DURATION = "duration"
+DISTANCE = "distance"
+ANGLE = "angle"
+MILLISECONDS = "milliseconds"
+MICROSECONDS = "microseconds"
+
+LEFT_X = "left_x"
+LEFT_Y = "left_y"
+LEFT_PUPIL = "left_pupil"
+
+RIGHT_X = "right_x"
+RIGHT_Y = "right_y"
+RIGHT_PUPIL = "right_pupil"
+
+ALL = "all"
+UNDEFINED = "undefined"
+BLINK = "blink"
+SACCADE = "saccade"
+FIXATION = "fixation"
