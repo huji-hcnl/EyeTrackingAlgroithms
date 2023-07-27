22faf5a0338eb5170477086d5498a2c437d63032
JonNir <jonathan.nir@mail.huji.ac.il>
2023-07-27 17:45:17 +0300

    add util func

--- Utils/array_utils.py
+++ Utils/array_utils.py
@@ -1,11 +1,26 @@
 import numpy as np
+from typing import List
 
 
 def is_one_dimensional(arr) -> bool:
+    """ Returns true if the array's shape is (n,) or (1, n) or (n, 1) """
     arr = np.asarray(arr)
     if arr.ndim == 1:
         return True
     if arr.ndim == 2 and min(arr.shape) == 1:
         return True
     return False
+
+
+def get_chunk_indices(arr) -> List[np.ndarray]:
+    """
+    Given a 1D array with multiple values, returns a list of arrays, where each array contains the indices of
+    a different "chunk", i.e. a sequence of the same value.
+    """
+    if not is_one_dimensional(arr):
+        raise ValueError("arr must be one-dimensional")
+    indices = np.arange(len(arr))
+    split_on = np.nonzero(np.diff(arr))[0] + 1  # +1 because we want to include the last index of each chunk
+    chunk_indices = np.split(indices, split_on)
+    return chunk_indices
 
