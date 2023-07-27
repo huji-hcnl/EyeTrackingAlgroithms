9e0adc852885c04ad12e9efa0a096333752fa319
JonNir <jonathan.nir@mail.huji.ac.il>
2023-07-27 12:49:06 +0300

    add function `calculate_velocities` in pixel space

--- __tests__/__unit_tests__/test_pixel_utils.py
+++ __tests__/__unit_tests__/test_pixel_utils.py
@@ -25,6 +25,31 @@ class TestPixelUtils(unittest.TestCase):
         y_coords4 = y_coords1[:-1].copy()
         self.assertRaises(AssertionError, pixel_utils.calculate_euclidean_distances, x_coords, y_coords4)
 
+    def test_calculate_velocities(self):
+        sqrt2 = np.sqrt(2)
+        x_coords = np.arange(0, 5)
+        y_coords1 = np.arange(0, 5)
+        t_coords1 = np.arange(5)
+        self.assertTrue(np.array_equal(np.array([np.nan, sqrt2, sqrt2, sqrt2, sqrt2]),
+                                       pixel_utils.calculate_velocities(x_coords, y_coords1, t_coords1),
+                                       equal_nan=True))
+        y_coords2 = np.zeros_like(x_coords)
+        self.assertTrue(np.array_equal(np.array([np.nan, 1, 1, 1, 1]),
+                                       pixel_utils.calculate_velocities(x_coords, y_coords2, t_coords1),
+                                       equal_nan=True))
+        t_coords2 = t_coords1 * 2
+        self.assertTrue(np.array_equal(np.array([np.nan, 0.5, 0.5, 0.5, 0.5]),
+                                       pixel_utils.calculate_velocities(x_coords, y_coords2, t_coords2),
+                                       equal_nan=True))
+        y_coords3 = y_coords1.copy().astype(float)
+        y_coords3[2] = np.nan
+        print(pixel_utils.calculate_velocities(x_coords, y_coords3, t_coords1))
+        self.assertTrue(np.array_equal(np.array([np.nan, sqrt2, np.nan, np.nan, sqrt2]),
+                                       pixel_utils.calculate_velocities(x_coords, y_coords3, t_coords1),
+                                       equal_nan=True))
+        t_coords3 = t_coords1[:-1].copy()
+        self.assertRaises(AssertionError, pixel_utils.calculate_velocities, x_coords, y_coords1, t_coords3)
+
     def test_calculate_azimuth(self):
         # angles are counter-clockwise from the positive x-axis, with y-axis pointing down
         self.assertEqual(0, pixel_utils.calculate_azimuth(p1=(0, 0), p2=(0, 0), use_radians=False))
