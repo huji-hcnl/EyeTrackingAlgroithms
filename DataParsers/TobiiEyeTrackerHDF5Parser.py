b7a1accb9ebd2e334c2de07f06637d718ab01792
JonNir <jonathan.nir@mail.huji.ac.il>
2023-07-30 17:28:42 +0300

    start implementation

--- DataParsers/TobiiEyeTrackerHDF5Parser.py
+++ DataParsers/TobiiEyeTrackerHDF5Parser.py
@@ -1,10 +1,34 @@
+import os
+import h5py as h5
+import pandas as pd
+
 from DataParsers.BaseEyeTrackerParser import BaseEyeTrackerParser
 
 
 class TobiiEyeTrackerHDF5Parser(BaseEyeTrackerParser):
     """
     Parses eye-tracking data based on the HDF5 format exported by Tobii eye-tracker and PsychoPy.
+    See additional information here: https://psychopy.org/hardware/eyeTracking.html#what-about-the-data
     """
     # TODO: implement this parser
-    raise NotImplementedError("This parser is not implemented yet.")
+
+    @staticmethod
+    def tobii_hdf5_to_dataframe(input_path: str):
+        if not os.path.exists(input_path):
+            raise FileNotFoundError(f'File not found: {input_path}')
+        with h5.File(input_path, 'r') as f:
+            dataset = f['data_collection']['events']['eyetracker']['BinocularEyeSampleEvent']
+            colnames = dataset.dtype.names
+            data_dict = {col: [] for col in colnames}
+            for row in dataset:
+                for i, col in enumerate(colnames):
+                    data_dict[col].append(row[i])
+        return pd.DataFrame(data_dict)
+
+    raise NotImplementedError
+
+
+
+
+
 
