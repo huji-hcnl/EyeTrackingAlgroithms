4cf5a3628f7a502d2a9d0a1dd053e328f090eaf6
JonNir <jonathan.nir@mail.huji.ac.il>
2023-07-30 13:14:59 +0300

    implement `_verify_inputs` for EngbertDetector.py

--- Detectors/EngbertDetector.py
+++ Detectors/EngbertDetector.py
@@ -1,5 +1,5 @@
 import numpy as np
-from typing import List
+from typing import List, Tuple
 
 from Detectors.BaseDetector import BaseDetector
 from GazeEvents.GazeEventTypeEnum import GazeEventTypeEnum
@@ -22,7 +22,27 @@ class EngbertDetector(BaseDetector):
     3. Calculate the noise threshold as the multiple of the median-standard-deviation with the constant lambda_noise_threshold
     4. Identify saccade candidates as samples with velocity greater than the noise threshold
     """
+    _LAMBDA_NOISE_THRESHOLD = 5  # default value used in the original paper
+    _DERIVATION_WINDOW_SIZE = 3  # default value used in the original paper
+
+    def __init__(self, sr: float,
+                 lambda_noise_threshold: float = _LAMBDA_NOISE_THRESHOLD,
+                 derivation_window_size: int = _DERIVATION_WINDOW_SIZE):
+        super().__init__(sr)
+        if lambda_noise_threshold <= 0:
+            raise ValueError("lambda_noise_threshold must be positive")
+        if derivation_window_size <= 0:
+            raise ValueError("derivation_window_size must be positive")
+        self._lambda_noise_threshold = lambda_noise_threshold
+        self._derivation_window_size = derivation_window_size
 
     def _identify_event_candidates(self, x: np.ndarray, y: np.ndarray) -> List[GazeEventTypeEnum]:
         raise NotImplementedError
+
+    def _verify_inputs(self, x: np.ndarray, y: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        x, y = super()._verify_inputs(x, y)
+        if len(x) < 2 * self._derivation_window_size:
+            raise ValueError(
+                f"x and y must be of length at least 2 * derivation_window_size (={2 * self._derivation_window_size})")
+        return x, y
 
