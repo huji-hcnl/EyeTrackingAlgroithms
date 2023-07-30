a98aa680b143d045c7884606f987eab4b1a45752
JonNir <jonathan.nir@mail.huji.ac.il>
2023-07-30 14:56:37 +0300

    add Engbert helper methods + tests

--- Detectors/EngbertDetector.py
+++ Detectors/EngbertDetector.py
@@ -14,7 +14,6 @@ class EngbertDetector(BaseDetector):
     Implementation is based on the following repositories:
         - https://github.com/Yuvishap/Gaze-Project/blob/master/Gaze/src/pre_processing/business/EngbertFixationsLogic.py
         - https://github.com/odedwer/EyelinkProcessor/blob/master/SaccadeDetectors.py
-        - https://github.com/esdalmaijer/PyGazeAnalyser/blob/master/pygazeanalyser/detectors.py
 
     General algorithm:
     1. Calculate the velocity of the gaze data in both axes
@@ -23,7 +22,7 @@ class EngbertDetector(BaseDetector):
     4. Identify saccade candidates as samples with velocity greater than the noise threshold
     """
     _LAMBDA_NOISE_THRESHOLD = 5  # default value used in the original paper
-    _DERIVATION_WINDOW_SIZE = 3  # default value used in the original paper
+    _DERIVATION_WINDOW_SIZE = 2  # default value used in the original paper
 
     def __init__(self, sr: float,
                  lambda_noise_threshold: float = _LAMBDA_NOISE_THRESHOLD,
@@ -34,7 +33,7 @@ class EngbertDetector(BaseDetector):
         if derivation_window_size <= 0:
             raise ValueError("derivation_window_size must be positive")
         self._lambda_noise_threshold = lambda_noise_threshold
-        self._derivation_window_size = derivation_window_size
+        self._derivation_window_size = round(derivation_window_size)
 
     def _identify_event_candidates(self, x: np.ndarray, y: np.ndarray) -> List[GazeEventTypeEnum]:
         raise NotImplementedError
@@ -45,4 +44,40 @@ class EngbertDetector(BaseDetector):
             raise ValueError(
                 f"x and y must be of length at least 2 * derivation_window_size (={2 * self._derivation_window_size})")
         return x, y
+
+    def _calculate_axial_velocity(self, arr) -> np.ndarray:
+        """
+        Calculates the velocity along a single axis, based on the algorithm described in the original paper:
+        1. Sum values in a window of size window_size, *before* the current sample:
+            sum_before = arr(t-1) + arr(t-2) + ... + arr(t-ws)
+        2. Sum values in a window of size window_size, *after* the current sample
+            sum_after = arr(t+1) + arr(t+2) + ... + arr(t+ws)
+        3. Calculate the difference between the two sums
+            diff = sum_after - sum_before
+        4. Divide by the time-difference, calculated as `sampling_rate` / (2 * `window_size`)
+            velocity = diff * (sampling_rate / (2 * (window_size + 1))
+        5. For the first and last `window_size` samples, the velocity is np.nan
+        """
+        arr_copy = np.copy(arr)
+        ws = self._derivation_window_size
+        velocities = np.full_like(arr_copy, np.nan)
+        for t in range(ws, len(arr_copy) - ws):
+            sum_before = np.sum(arr_copy[t - ws:t])
+            sum_after = np.sum(arr_copy[t + 1:t + ws + 1])
+            diff = sum_after - sum_before
+            velocities[t] = diff * (self._sr / (2 * (ws + 1)))
+        return velocities
+
+
+    @staticmethod
+    def _median_standard_deviation(arr) -> float:
+        """
+        Calculates the median standard deviation of the given array
+        """
+        squared_median = np.power(np.nanmedian(arr), 2)
+        median_of_squares = np.nanmedian(np.power(arr, 2))
+        sd = np.sqrt(median_of_squares - squared_median)
+        return float(np.nanmax([sd, 1e-6]))
+
+
 
