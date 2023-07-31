aee7ac446d54b3e8001765984b8e25937433661c
JonNir <jonathan.nir@mail.huji.ac.il>
2023-07-31 14:11:19 +0300

    implement HDF5 parser (part 1)

--- DataParsers/TobiiEyeTrackerHDF5Parser.py
+++ DataParsers/TobiiEyeTrackerHDF5Parser.py
@@ -1,6 +1,8 @@
 import os
 import h5py as h5
+import numpy as np
 import pandas as pd
+from typing import Set, Union, Optional
 
 from DataParsers.BaseEyeTrackerParser import BaseEyeTrackerParser
 
@@ -10,14 +12,21 @@ class TobiiEyeTrackerHDF5Parser(BaseEyeTrackerParser):
     Parses eye-tracking data based on the HDF5 format exported by Tobii eye-tracker and PsychoPy.
     See additional information here: https://psychopy.org/hardware/eyeTracking.html#what-about-the-data
     """
-    # TODO: implement this parser
+
+    def parse(self, input_path: str, *args) -> pd.DataFrame:
+        raise NotImplementedError
 
     @staticmethod
     def tobii_hdf5_to_dataframe(input_path: str):
         """
         Reads the raw HDF5 file exported by Tobii eye-tracker and returns a pandas DataFrame.
-        See information about the file structure and data format here:
-        https://psychopy.org/hardware/eyeTracking.html#what-about-the-data
+        See aditional resources:
+        - file structure (hdf5): https://psychopy.org/hardware/eyeTracking.html#what-about-the-data
+        - data format: https://psychopy.org/api/iohub/device/eyetracker_interface/Tobii_Implementation_Notes.html
+        - PsychoPy's IOHub code:
+            * event constants: https://github.com/psychopy/versions/blob/master/psychopy/iohub/constants.py#L81
+            * eye tracker constants: https://github.com/psychopy/versions/blob/master/psychopy/iohub/constants.py#L985
+            * eye tracker binocular event: https://github.com/psychopy/versions/blob/master/psychopy/iohub/devices/eyetracker/eye_events.py#L330
 
         :param input_path: path to the HDF5 file
         :return: pandas DataFrame
@@ -33,6 +42,60 @@ class TobiiEyeTrackerHDF5Parser(BaseEyeTrackerParser):
                 for i, col in enumerate(colnames):
                     data_dict[col].append(row[i])
         return pd.DataFrame(data_dict)
+
+    @classmethod
+    def MISSING_VALUES(cls) -> Set[Union[int, float, str]]:
+        return {np.nan, None}
+
+    @classmethod
+    def TRIAL_COLUMN(cls) -> Optional[str]:
+        # column name for trial number
+        return None
+
+    @classmethod
+    def SECONDS_COLUMN(cls) -> Optional[str]:
+        # column name for time in seconds
+        return "time"
+
+    @classmethod
+    def MILLISECONDS_COLUMN(cls) -> Optional[str]:
+        # column name for time in milliseconds
+        return None
+
+    @classmethod
+    def MICROSECONDS_COLUMN(cls) -> Optional[str]:
+        # column name for time in microseconds
+        return None
+
+    @classmethod
+    def LEFT_X_COLUMN(cls) -> Optional[str]:
+        # column name for left eye x coordinate
+        return "left_gaze_x"
+
+    @classmethod
+    def LEFT_Y_COLUMN(cls) -> Optional[str]:
+        # column name for left eye y coordinate
+        return "left_gaze_y"
+
+    @classmethod
+    def LEFT_PUPIL_COLUMN(cls) -> Optional[str]:
+        # column name for left eye pupil diameter
+        return "left_pupil_measure_1"
+
+    @classmethod
+    def RIGHT_X_COLUMN(cls) -> Optional[str]:
+        # column name for right eye x coordinate
+        return "right_gaze_x"
+
+    @classmethod
+    def RIGHT_Y_COLUMN(cls) -> Optional[str]:
+        # column name for right eye y coordinate
+        return "right_gaze_y"
+
+    @classmethod
+    def RIGHT_PUPIL_COLUMN(cls) -> Optional[str]:
+        # column name for right eye pupil diameter
+        return "right_pupil_measure_1"
 
     raise NotImplementedError
 
