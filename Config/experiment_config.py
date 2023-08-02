c12042ab3e80f0c64d019f5a9934985176e05f23
JonNir <jonathan.nir@mail.huji.ac.il>
2023-08-02 12:43:08 +0300

    take triggers from config file

--- Config/experiment_config.py
+++ Config/experiment_config.py
@@ -4,6 +4,7 @@ This file contains the configuration for each specific experiment.
 
 import os
 from Config.ScreenMonitor import ScreenMonitor
+from Config.ExperimentTriggerEnum import ExperimentTriggerEnum
 
 # DIRECTORIES
 BASE_DIR = ""
@@ -13,4 +14,5 @@ OUTPUT_DIR = os.path.join(BASE_DIR, "Results")
 
 # GLOBAL VARIABLES
 SCREEN_MONITOR: ScreenMonitor = ScreenMonitor.from_default()  # global variable: screen monitor object
+TRIGGERS = ExperimentTriggerEnum  # global variable: triggers enum
 EXPERIMENT_SPECIFIC_VARIABLES = []  # additional variable recorded in the experiment and extracted from the raw data
