329730a039bbc22eaa9bb225acb78cb4cc1ab897
JonNir <jonathan.nir@mail.huji.ac.il>
2023-08-02 12:05:37 +0300

    create `find_sequences_in_sparse_array`

--- Utils/array_utils.py
+++ Utils/array_utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-from typing import List, Set
+from typing import List, Tuple
 
 
 def is_one_dimensional(arr) -> bool:
@@ -23,3 +23,25 @@ def get_chunk_indices(arr) -> List[np.ndarray]:
     split_on = np.nonzero(np.diff(arr))[0] + 1  # +1 because we want to include the last index of each chunk
     chunk_indices = np.split(indices, split_on)
     return chunk_indices
+
+
+def find_sequences_in_sparse_array(sparse_array: np.ndarray, sequence: np.ndarray) -> List[Tuple[int, int]]:
+    """
+    Finds all occurrences of the given sequence in the given sparse array, while ignoring intermediate NaN values.
+    :param sparse_array: array to search in, may contain NaN values
+    :param sequence: sequence to search for
+    :return: list of (start_idx, end_idx) tuples for each occurrence of the sequence in the array
+
+    see examples in https://stackoverflow.com/a/76812495/8543025
+    """
+    from numpy.lib.stride_tricks import sliding_window_view as swv
+    if not is_one_dimensional(sparse_array):
+        raise ValueError("arr must be one-dimensional")
+    n = len(sequence)
+    non_nan_idxs = np.where(~np.isnan(sparse_array))[0]
+    if len(non_nan_idxs) < n:
+        return []
+    swv_non_nan_array = swv(sparse_array[non_nan_idxs], n)
+    is_sequence = np.all(swv_non_nan_array == sequence, axis=1)
+    start_end_idxs = list(zip(non_nan_idxs[:1-n][is_sequence], non_nan_idxs[n-1:][is_sequence]))
+    return start_end_idxs
