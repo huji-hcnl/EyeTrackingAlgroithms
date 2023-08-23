5e28883c231f40df3d44ee9342151f19119e220d
JonNir <jonathan.nir@mail.huji.ac.il>
2023-08-23 14:29:38 +0300

    create DataSet loaders

---
+++ DataSets/BaseDataSetLoader.py
@@ -0,0 +1,63 @@
+import os
+import pandas as pd
+import requests as req
+from abc import ABCMeta, abstractmethod
+from typing import List
+
+
+class BaseDataSetLoader(ABCMeta):
+    _URL: str = None
+    _ARTICLE: str = None
+
+    @classmethod
+    def from_remote(cls) -> pd.DataFrame:
+        """ Loads the dataset from a remote URL """
+        response = req.get(cls._URL)
+        if response.status_code != 200:
+            raise RuntimeError(f"Failed to download dataset from {cls._URL}")
+        return cls._from_remote_impl(response)
+
+    @classmethod
+    def from_local(cls, local_path: str) -> pd.DataFrame:
+        """ Loads the dataset from a local file """
+        if not os.path.exists(local_path):
+            raise FileNotFoundError(f"Could not find local file: {local_path}")
+        return cls._from_local_impl(local_path)
+
+    @classmethod
+    def save_to_pickle(cls, df: pd.DataFrame, path_file: str = None) -> None:
+        if path_file is None:
+            path_file = f"{cls.__name__}.pkl"
+        if not path_file.endswith(".pkl"):
+            path_file += ".pkl"
+        df.to_pickle(path_file)
+
+    @classmethod
+    def url(cls) -> str:
+        return cls._URL
+
+    @classmethod
+    def article(cls) -> str:
+        return cls._ARTICLE
+
+    @classmethod
+    @abstractmethod
+    def columns(cls) -> List[str]:
+        raise NotImplementedError
+
+    @classmethod
+    @abstractmethod
+    def _from_remote_impl(cls, response: req.Response) -> pd.DataFrame:
+        raise NotImplementedError
+
+    @classmethod
+    @abstractmethod
+    def _from_local_impl(cls, local_path: str) -> pd.DataFrame:
+        raise NotImplementedError
+
+    def __init_subclass__(cls, **kwargs):
+        if cls._URL is None:
+            raise NotImplementedError(f"Class {cls.__name__} must implement class attribute `_URL`")
+        if cls._ARTICLE is None:
+            raise NotImplementedError(f"Class {cls.__name__} must implement class attribute `_ARTICLE`")
+
