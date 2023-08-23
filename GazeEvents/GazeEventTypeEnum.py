0f849204cffc267253fedd84629e764bbc52554a
JonNir <jonathan.nir@mail.huji.ac.il>
2023-08-23 14:11:03 +0300

    add `get_event_type` method

--- GazeEvents/GazeEventTypeEnum.py
+++ GazeEvents/GazeEventTypeEnum.py
@@ -1,4 +1,5 @@
 from enum import IntEnum
+from typing import Union
 
 
 class GazeEventTypeEnum(IntEnum):
@@ -8,3 +9,21 @@ class GazeEventTypeEnum(IntEnum):
     PSO = 3
     SMOOTH_PURSUIT = 4
     BLINK = 5
+
+
+def get_event_type(v: Union[GazeEventTypeEnum, int, str], safe: bool = True) -> GazeEventTypeEnum:
+    try:
+        if type(v) not in [GazeEventTypeEnum, int, str]:
+            raise TypeError(f"Incompatible type: {type(v)}")
+        if type(v) is GazeEventTypeEnum:
+            return v
+        if type(v) is int:
+            return GazeEventTypeEnum(v)
+        if type(v) is str:
+            return GazeEventTypeEnum[v.upper()]
+        return GazeEventTypeEnum(v)
+    except Exception as e:
+        if safe and (type(e) is ValueError or type(e) is TypeError):
+            return GazeEventTypeEnum.UNDEFINED
+        raise e
+
